5f47390eff6ae10a5293799844e554e7c214f485
sakonpatsamit <95245596+sakonpatsamit@users.noreply.github.com>
2022-03-11 13:09:13 +0100

    finished

---
+++ script.js
@@ -0,0 +1,38 @@
+// question 1
+var dogName = "Casper";
+console.log(dogName);
+var numberTen = 10;
+console.log(numberTen);
+var booleanValue = true;
+console.log(booleanValue);
+
+// question 2
+
+var firstName = "Sakon";
+var lastName = "Patsamit";
+var space = " ";
+
+var fullName = firstName + space + lastName;
+console.log(fullName);
+
+// question 3
+
+var animal = "frog";
+var typeOfFrog = typeof animal;
+console.log("The type of frog is", typeOfFrog);
+
+// question 4
+
+var orderHasShipped = true;
+
+if (orderHasShipped === true) {
+  console.log("The order shipped");
+} else {
+  console.log("The order did not ship");
+}
+
+// question 5
+
+for (var i = 7; i <= 13; i++) {
+  console.log(i);
+}
